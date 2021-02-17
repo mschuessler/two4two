f5da7ba97f55cfe91eaf4b6f7371854a4ece39ad
berleon <github@leon-sixt.de>
2021-02-17 15:28:27 +0100

    Add discrete wrapper class
    The scipy.stats.rv_discrete class only support integers.
    This class supports any values.

--- two4two/utils.py
+++ two4two/utils.py
@@ -1,5 +1,64 @@
+"""utility functions."""
+
+from typing import Any, Dict, Sequence, TypeVar, Union
+
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
+
+
+T = TypeVar('T')
+
+
+class discrete():
+    """Wrapper around ``scypi.stats.rv_discrete`` to support more than ints.
+
+    Attrs:
+        values: The values of the discrete distribution.
+        rv_discrete: The ``scypi.stats.rv_discrete`` distributon.
+    """
+    def __init__(
+            self,
+            value_to_probs: Dict[T, float],
+            **stats_kwargs: Dict[str, Any]):
+        """A discrete distribution with any values.
+
+        Args:
+            value_to_probs: A mapping of the distribution's values to probabilities.
+            **stats_kwargs: Passed on to ``scipy.stats.rv_discrete``.
+        """
+        self.values = list(value_to_probs.keys())
+        value_indicies = list(range(len(self.values)))
+        probs = list(value_to_probs.values())
+        self.rv_discrete = scipy.stats.rv_discrete(
+            values=[value_indicies, probs], **stats_kwargs)
+
+    def pmf(self,
+            k: Union[T, Sequence[T]],
+            *args: Sequence[Any],
+            **kwargs: Dict[str, Any]
+            ) -> Sequence[float]:
+        """Probability mass function."""
+        return np.exp(self.logpmf(k, *args, **kwargs))
+
+    def logpmf(self,
+               k: Union[T, Sequence[T]],
+               *args: Sequence[Any],
+               **kwargs: Dict[str, Any]
+               ) -> Sequence[float]:
+        """Log Probability mass function."""
+        if k in self.values:  # a single k
+            return self.rv_discrete.logpmf(self.values.index(k))
+        else:  # a sequence of k's
+            return self.rv_discrete.logpmf([self.values.index(k_item) for k_item in k])
+
+    def rvs(self,
+            *args: Sequence[Any],
+            **kwargs: Dict[str, Any]
+            ) -> T:
+        """Samples from distribution."""
+        return self.values[self.rv_discrete.rvs(*args, **kwargs)]
+
 
 class ColorGenerator():
 
