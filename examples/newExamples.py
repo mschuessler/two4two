43fb84f5e3b68bcf05c2d6f5ce73467eb4c8c662
Martin Schuessler <dev@mschuessler.de>
2021-02-24 00:59:14 +0100

    saving bulk code

--- examples/newExamples.py
+++ examples/newExamples.py
@@ -1,9 +1,9 @@
-from two4two import blender
-from two4two import scene_parameters
 from matplotlib import pyplot as plt
-import os
+import matplotlib.gridspec as gridspec
 import numpy as np
 
+from two4two import blender
+from two4two.utils import splitStickyStretchy
 
 sampler = scene_parameters.ColorBiasedSceneParameterSampler()
 
@@ -18,6 +18,51 @@ sampler = scene_parameters.ColorBiasedSceneParameterSampler()
 # num_rows = np.ceil(num_samples_per_class / num_cols_per_class)
 # ######
 
+params = [sampler.sample() for _ in range(30)]
+len(params)
+len(sticky_params)
+len(stretchy_params)
+enforce_equal_class_distribution = False
+num_cols_per_class = 3
+max(len(sticky_params), len(stretchy_params))
+np.ceil(41 / 3)
+num_rows
+np.ceil(5).astype('int')
+
+sticky_ax
+
+
+
+def renderGrid(params, num_cols_per_class=3, enforce_equal_class_distribution=True):
+    sticky_params, stretchy_params = splitStickyStretchy(params)
+
+    if enforce_equal_class_distribution:
+        num_rows = np.floor(min(len(sticky_params), len(stretchy_params)) / num_cols_per_class)
+        num_samples_per_class = int(num_rows * num_cols_per_class)
+        sticky_params, stretchy_params = splitStickyStretchy(params, num_samples_per_class)
+    else:
+        num_rows = np.ceil(max(len(sticky_params), len(stretchy_params)) / num_cols_per_class)
+
+    num_rows = num_rows.astype('int')
+
+    fig, ax = plt.subplots( nrows=num_rows, ncols=num_cols_per_class * 2)
+    fig.subplots_adjust(wspace=0, hspace=0)
+    sticky_ax = ax[:, :num_cols_per_class].flatten().tolist()[::-1]
+    stretchy_ax = ax[:, num_cols_per_class:].flatten().tolist()[::-1]
+
+    for (img, param) in blender.render(
+            params=sticky_params + stretchy_params,
+            chunk_size=num_cols_per_class,
+            download_blender=True):
+        ax1 = sticky_ax.pop() if param.obj_name == 'sticky' else stretchy_ax.pop()
+        ax1.axis('off')
+        ax1.set_aspect('equal')
+        ax1.imshow(img)
+
+
+renderGrid(params)
+
+
 
 def showExamplesForSampler(sampler=scene_parameters.SampleSceneParameters, num_rows=10, num_cols_per_class=3):
     num_samples_per_class = num_rows * num_cols_per_class
@@ -27,7 +72,7 @@ def showExamplesForSampler(sampler=scene_parameters.SampleSceneParameters, num_r
         [p for p in sampled_params if p.obj_name == 'stretchy'][:num_samples_per_class]
 
     # Creating a matrix of indicies for the subplot
-    plot_index = np.arange(num_samples_per_class * 2).reshape(-1, num_cols_per_class * 2) + 1
+    plot_index = np.arange(num_samples_per_class * 2).reshape(-1, num_cols_per_class * 2)
     # Left indicies are used for sticky
     sticky_index = plot_index[:, :num_cols_per_class]
     # Right indicies are used for stretchy
@@ -36,7 +81,9 @@ def showExamplesForSampler(sampler=scene_parameters.SampleSceneParameters, num_r
     sticky_index = sticky_index.flatten().tolist()[::-1]
     stretchy_index = stretchy_index.flatten().tolist()[::-1]
 
-    plt.figure(figsize=(num_rows, num_cols_per_class * 2), dpi=200)
+    plt.figure(figsize=(num_cols_per_class * 2, num_rows), dpi=200)
+    gs1 = gridspec.GridSpec(num_rows, num_cols_per_class * 2, wspace=0.005, hspace=0.005)
+    # gs1.update(wspace=0.005, hspace=0.005)
     # plt.subplots(nrows=...)
     for (img, param) in blender.render(
             params=selected_params,
@@ -44,9 +91,10 @@ def showExamplesForSampler(sampler=scene_parameters.SampleSceneParameters, num_r
             download_blender=True):
 
         index = sticky_index.pop() if param.obj_name == 'sticky' else stretchy_index.pop()
-        plt.subplot(num_rows, num_cols_per_class * 2, index)
-        plt.subplots_adjust(wspace=0, hspace=0)
-        plt.axis('off')
-        plt.imshow(img)
+        ax1 = plt.subplot(gs1[index])
+        ax1.axis('off')
+        ax1.set_aspect('equal')
+        ax1.imshow(img)
+
 
 showExamplesForSampler(sampler)
