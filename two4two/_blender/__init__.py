d6a3e2bf97bde08438971ac61c5ff4ec09e84d9e
berleon <github@leon-sixt.de>
2021-03-02 15:45:10 +0100

    Run flake8 also on the _blender submodule

--- two4two/_blender/__init__.py
+++ two4two/_blender/__init__.py
@@ -1,3 +1,5 @@
 """Modules and scripts for the blender python only.
 
-You should never import something from this module ever."""
+This module only contains functions for the rendering. You should not import
+something from this module.
+"""
