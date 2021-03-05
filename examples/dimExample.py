d144740aa33da4acf53c8fea8aa4ff0fdd56141e
Martin Schuessler <github@mschuessler.de>
2021-03-05 13:22:08 +0100

    Extending example

--- examples/dimExample.py
+++ examples/dimExample.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from two4two import scene_parameters
 from two4two import utils
 from two4two.plotvis import render_grid
@@ -12,9 +14,20 @@ base_param.check_values()
 params = [base_param.clone() for i in range(5)]
 [sampler.sample_spherical(param) for param in params]
 render_grid(params,equal_class_distribution=False)
-
-base_param = sampler.sample()
-base_param.check_values()
+sorted(params,key=lambda p: p.spherical)
 params = [base_param.clone() for i in range(5)]
 [sampler.sample_obj_rotation(param) for param in params]
 render_grid(params,equal_class_distribution=False)
+
+
+params = range()
+
+
+def resample_cloes(samplerFunction: [scene_parameters.Distribution],
+param: scene_parameters.SceneParameters, num_clones: int = 5) -> List[scene_parameters.SceneParameters]:
+    clones = []
+    for i in range(num_clones):
+        clone = param.clone()
+        samplerFunction(clone)
+        clones.append(clone)
+    return clones
