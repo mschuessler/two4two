9e4085b7addefbf7b56a716cabcacfaa639b7e72
Martin Schuessler <github@mschuessler.de>
2021-03-05 13:57:00 +0100

    Extending dim example

--- examples/dimExample.py
+++ examples/dimExample.py
@@ -1,33 +1,30 @@
-from typing import List
+from typing import Callable, List
 
 from two4two import scene_parameters
 from two4two import utils
 from two4two.plotvis import render_grid
 
-my_scene = scene_parameters.SceneParameters()
-my_scene.check_values()
+# Better than sampeling: initlilase with default
+# my_scene = scene_parameters.SceneParameters()
 
 sampler = scene_parameters.SampleSceneParameters()
+sampler.obj_name = 'sticky'
 
 base_param = sampler.sample()
-base_param.check_values()
-params = [base_param.clone() for i in range(5)]
-[sampler.sample_spherical(param) for param in params]
-render_grid(params,equal_class_distribution=False)
-sorted(params,key=lambda p: p.spherical)
-params = [base_param.clone() for i in range(5)]
-[sampler.sample_obj_rotation(param) for param in params]
-render_grid(params,equal_class_distribution=False)
 
 
-params = range()
-
-
-def resample_cloes(samplerFunction: [scene_parameters.Distribution],
-param: scene_parameters.SceneParameters, num_clones: int = 5) -> List[scene_parameters.SceneParameters]:
+def resample_clones(samplerFunction:
+                    Callable[[scene_parameters.SceneParameters], scene_parameters.SceneParameters],
+                    param: scene_parameters.SceneParameters, num_clones: int = 5
+                    ) -> List[scene_parameters.SceneParameters]:
     clones = []
     for i in range(num_clones):
         clone = param.clone()
         samplerFunction(clone)
+        clone.check_values()
         clones.append(clone)
     return clones
+
+var_color_params = resample_clones(sampler.sample_obj_color, base_param, num_clones = 10)
+
+render_grid(var_color_params, equal_class_distribution=False)
