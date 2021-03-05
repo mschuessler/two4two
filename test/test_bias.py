0ac453c39eee305d441f1c876da5e80c80844f26
berleon <github@leon-sixt.de>
2021-03-05 16:08:10 +0100

    add __init__.py export

--- test/test_bias.py
+++ test/test_bias.py
@@ -5,13 +5,13 @@ import random
 
 import pytest
 
-from two4two import bias
+import two4two
 from two4two import utils
 
 
 def test_generic_sampler():
     """Tests if generic sample can handle all its intended types."""
-    sampler = bias.Sampler()
+    sampler = two4two.Sampler()
     scipy_trunc_normal = utils.truncated_normal(0, 0.5, 0, 1)
     py_uniform = random.random
     test_dict = {'sticky': scipy_trunc_normal, 'stretchy': py_uniform, 'ignore': None}
@@ -22,7 +22,7 @@ def test_generic_sampler():
     assert isinstance(sampler._sample('stretchy', test_dict, size=5), list)
 
     with pytest.raises(KeyError):
-        bias.Sampler._sample('ronny', test_dict)
+        two4two.Sampler._sample('ronny', test_dict)
 
-    colorBiasedSample = bias.ColorBiasedSampler()
+    colorBiasedSample = two4two.ColorBiasedSampler()
     colorBiasedSample.sample()
