9c33e45015b6b158bf36170a1371a6274561018b
Martin Schuessler <github@mschuessler.de>
2021-03-22 21:38:54 +0100

    tidy up

--- examples/train_resnet.py
+++ examples/train_resnet.py
@@ -1,41 +1,36 @@
+import os
+import pathlib
+
+
+from keras_preprocessing.image import ImageDataGenerator
+import pandas as pd
 import tensorflow as tf
 import tensorflow.keras as keras
-from keras_preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
-from livelossplot import PlotLossesKerasTF
-import numpy as np
-import pandas as pd
-import json
-import pathlib
-import PIL
-import PIL.Image
-import os
 
-
-
-
-
-data_dir = os.path.join("datasets","high_variation")
+data_dir = os.path.join("datasets", "high_variation")
 data_dir = pathlib.Path(data_dir)
 
-train_dir = os.path.join(data_dir,"train")
+train_dir = os.path.join(data_dir, "train")
 train_df = pd.read_json(os.path.join(train_dir, "parameters.jsonl"), lines=True)
 train_df["filename"] = train_df["id"] + ".png"
 
 
-valid_dir = os.path.join(data_dir,"validation")
+valid_dir = os.path.join(data_dir, "validation")
 valid_df = pd.read_json(os.path.join(valid_dir, "parameters.jsonl"), lines=True)
 valid_df["filename"] = valid_df["id"] + ".png"
 
 
 # Assuming that 1/255 is a good idea as it was with MNIST
-datagen=ImageDataGenerator(rescale=1./255)
-train_generator=datagen.flow_from_dataframe(dataframe=train_df, directory=train_dir, x_col="filename", y_col="obj_name", batch_size=64)
-valid_generator=datagen.flow_from_dataframe(dataframe=valid_df, directory=valid_dir, x_col="filename", y_col="obj_name", batch_size=64)
-STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
-STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
+datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = datagen.flow_from_dataframe(dataframe=train_df, directory=train_dir,
+                                              x_col="filename", y_col="obj_name", batch_size=64)
+valid_generator = datagen.flow_from_dataframe(dataframe=valid_df, directory=valid_dir,
+                                              x_col="filename", y_col="obj_name", batch_size=64)
+STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
+STEP_SIZE_VALID = valid_generator.n // valid_generator.batch_size
 
-checkpoint_filepath = os.path.join("models","resnet50_highVariation")
+checkpoint_filepath = os.path.join("models", "resnet50_highVariation")
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_filepath,
     save_weights_only=True,
@@ -45,17 +40,18 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
 
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
-    base_model = keras.applications.resnet50.ResNet50(weights= None, pooling ='avg', include_top=False, input_shape= (128,128,3))
-    predictions = layers.Dense(2, activation= 'softmax')(base_model.output)
-    resnet_model = keras.models.Model(inputs = base_model.input, outputs = predictions)
+    base_model = keras.applications.resnet50.ResNet50(
+        weights=None, pooling='avg', include_top=False, input_shape=(128, 128, 3))
+    predictions = layers.Dense(2, activation='softmax')(base_model.output)
+    resnet_model = keras.models.Model(inputs=base_model.input, outputs=predictions)
 
     adam = keras.optimizers.Adam(lr=0.0003)
-    resnet_model.compile(optimizer= adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    resnet_model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
 resnet_model.fit(train_generator,
-                    steps_per_epoch=STEP_SIZE_TRAIN,
-                    validation_data=valid_generator,
-                    validation_steps=STEP_SIZE_VALID,
-                    epochs=30,
-                    callbacks = [model_checkpoint_callback]
-                    )
+                 steps_per_epoch=STEP_SIZE_TRAIN,
+                 validation_data=valid_generator,
+                 validation_steps=STEP_SIZE_VALID,
+                 epochs=30,
+                 callbacks=[model_checkpoint_callback]
+                 )
